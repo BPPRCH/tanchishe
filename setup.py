9b89f35548de94b985839b1a6f567ce087e0fd65
大方是小方 <www.haoshiyu@outlook.com>
2024-01-12 10:14:49 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='贪吃蛇',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='大方是小方',
+    author_email='www.haoshiyu@outlook.com',
+    description=''
+)
